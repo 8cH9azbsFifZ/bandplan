a69f203b24e47313d0c85a0113bfd77350beeeeb
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2012-10-22 23:37:56 +0200

    conversion script

---
+++ convert.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+infile=$1
+outfile=$infile.out
+cat $infile |awk '
+{
+	gsub("&deg;","°");
+	print;
+}
+' |awk '
+BEGIN{FS=";"}
+{
+	call=$1;qrg=$2;input=$3;locator=$4;info=$5;lat=$6;lon=$7;ctcss=$8;mode=$9;
+	if(qrg==""){next;}
+	gsub(",",".",qrg);
+	gsub(",",".",ctcss);
+	gsub(",",".",input);
+	print "<channel name=\""call"\" freq=\""qrg"\">";
+	if(input!=""){print "\t<repeater input=\""input"\"/>";}
+	print "\t<position locator=\""locator"\"/>";
+	print "\t<position lat=\""lat"\" lon=\""lon"\"/>";
+	print "\t<comment>"info"</comment>";
+	if (mode!="")
+	{
+		if (match(mode,"EL#") or match(mode,"el#"))
+		{			
+			node=mode;
+			gsub(".*EL#","",node);
+			gsub(".*el#","",node);
+			gsub(",.*","",node);
+			gsub("/.*","",node);
+			print "\t<echolink node=\""node"\"/>";
+		}
+		if (match(mode,"IL#") or match(mode,"il#"))
+		{			
+			node=mode;
+			gsub(".*IL#","",node);
+			gsub(".*il#","",node);
+			gsub("/.*","",node);
+			print "\t<ilrp node=\""node"\"/>";
+		}
+
+		print "\t<comment>"mode"</comment>";
+	}
+	if (ctcss!="")
+	{
+		print "\t<ctcss>"ctcss"</ctcss>";
+	}
+	print "\t<source href=\"http://www.darc.de/echolink-bin/relais.pl\"/>";
+	print "</channel>";
+}' >$outfile
+
