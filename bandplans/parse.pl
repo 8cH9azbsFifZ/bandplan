870bdf8faaf0ae4101e3885b617a0068e219158d
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2012-11-17 18:03:47 +0100

    change check ok

--- bandplans/parse.pl
+++ bandplans/parse.pl
@@ -13,12 +13,16 @@ use Data::Dumper;
 my %list = %{$data->{references}->{ref}};
 #print Dumper %list;
 
+$changed = 0;
 foreach my $ref (keys %list){
 	my $id = $list{$ref}{id};
 	my $md5 = $list{$ref}{md5};
 	my $href = $list{$ref}{href};
-	my $md5_check = `wget $href | md5`;
-	print "Reference $id $md5 $md5_check $ref $href\n";
+	my $md5_check = `wget -O - $href 2>/dev/null| md5`;
+	chomp ($md5_check);
+	$md5 == $md5_check and next;
+	$changed = 1;
+	print "Reference $id \"$ref\" has changed ($md5 != $md5_check ).\n";
 }
 
 
