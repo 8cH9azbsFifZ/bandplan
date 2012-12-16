f7d6aea5b4e643f3ce09e002b77d0de7463c28ed
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2012-12-16 20:58:59 +0100

    testing

---
+++ tools/freq_to_info.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -s
+$filename = "../bandplan/bandplan.xml";
+$filename or die;
+
+use XML::Simple;
+use Data::Dumper;
+
+$xml = new XML::Simple;
+$data = $xml->XMLin($filename);
+
+my %list = %{$data->{references}->{ref}};
+$changed = 0;
+foreach my $ref (keys %list){
+	my $id = $list{$ref}{id};
+	my $md5 = $list{$ref}{md5};
+	my $href = $list{$ref}{href};
+	my $localfile = $list{$ref}{localfile};
+	$localfile or next;
+	$localfile = $base_references_dir."/".$localfile; 
+	`wget -O $localfile $href`;
+	my $md5_check = `cat $localfile | md5`;
+	chomp ($md5_check);
+	$md5 == $md5_check and next;
+	$changed = 1;
+	print "Reference $id \"$ref\" has changed ($md5 != $md5_check ).\n";
+}
+$changed == 0 and print "No changes detected in references.\n";
+$changed == 1 and print "Changes detected in references.\n";
+exit $changed;
+
